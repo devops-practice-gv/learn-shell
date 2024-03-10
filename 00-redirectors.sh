9d7faf944758031508d571ec8dae52675955e45f
Gaurav <gauravenigma@gmail.com>
2024-03-10 22:12:18 +0000

    Redirectors &>

--- 00-redirectors.sh
+++ 00-redirectors.sh
@@ -1,2 +1,10 @@
 ls >/tmp/out
 cat /tme/out
+
+
+# > or 1> for output redirectors
+# 2> for error only redirectors
+# &> for ALL message redirectors
+
+# &>> will append the messages
+# &>/deev/null      This will not store any output and it will be purged.
